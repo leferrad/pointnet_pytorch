e65d7adaec93c7f4328af320f65857cc299fe6ad
Leandro Ferrado <leferrad@gmail.com>
2021-03-07 15:43:08 -0300

    Added message error in collect_indoor3d_data.py

--- data_preparation/collect_indoor3d_data.py
+++ data_preparation/collect_indoor3d_data.py
@@ -17,5 +17,5 @@ for anno_path in anno_paths:
         elements = anno_path.split('/')
         out_filename = elements[-3]+'_'+elements[-2]+'.npy' # Area_1_hallway_1.npy
         indoor3d_util.collect_point_label(anno_path, os.path.join(output_folder, out_filename), 'numpy')
-    except:
-        print(anno_path, 'ERROR!!')
+    except Exception as e:
+        print(anno_path, 'ERROR!! %s' % str(e))
